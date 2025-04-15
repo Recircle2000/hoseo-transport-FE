9920458278e918883d2cf5df17c8ff08422dabe9
Recircle2000 <ijw1004j@gmail.com>
2025-04-15 16:47:57 +0900

    ios빌드 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,7 +9,18 @@ import 'view/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  print("앱 시작");
+  await FlutterNaverMap().init(
+    clientId: 'v5ip94eaii',
+      onAuthFailed: (ex) => switch (ex) {
+        NQuotaExceededException(:final message) =>
+            print("사용량 초과 (message: $message)"),
+        NUnauthorizedClientException() ||
+        NClientUnspecifiedException() ||
+        NAnotherAuthFailedException() =>
+            print("인증 실패: $ex"),
+      }
+  );
   // 화면 자동 회전 비활성화 - 세로 모드만 허용
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
